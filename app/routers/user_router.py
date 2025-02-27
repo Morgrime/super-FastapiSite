bdf3d4c6a573c7d22a09839aa97655149880677f
Morgrime <daniel2000@mail.ru>
2025-02-27 18:34:20 +0500

    Added func to change password

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -4,9 +4,9 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 from utils.dependencies import get_current_user
 from database.session import get_session
-from database.crud import get_user_by_username
 from models.models import User
-from schemas.user_scheme import UserProfileResponse, UserWithProfileResponse
+from utils.security import change_password
+from schemas.user_scheme import UserProfileResponse, UserWithProfileResponse, ChangePassword
 
 
 router = APIRouter()
@@ -42,11 +42,15 @@ async def get_profile(current_user: dict = Depends(get_current_user),
     return response_data
 
 
-
-# @router.get("/profile", tags=["User"])
-# async def get_profile(current_user: dict = Depends(get_current_user),
-#                       session: AsyncSession = Depends(get_session)):
-#     user = await get_user_by_username(session, current_user["sub"])
-#     if not user:
-#         raise HTTPException(status_code=404, detail="Profiel not found")
-#     return user
+@router.post("/change_password", response_model=ChangePassword, tags=["User"])
+async def change_password_route(data: ChangePassword,
+                                current_user: User = Depends(get_current_user),
+                                session: AsyncSession = Depends(get_session)):
+    try:
+        user = await change_password(session,
+                                     current_user,
+                                     data.old_password,
+                                     data.new_password)
+        return user
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- app/utils/security.py
+++ app/utils/security.py
@@ -1,13 +1,29 @@
 from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
+from models.models import User
 
 pwd_context = CryptContext(schemes=["bcrypt"])
 
 
-# хэш пароля
+# тут происходит хэширование пароля
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
-# проверка пароля
+# тут происходит проверка правильности ввода пароля
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+# тут происходит смена пароля
+async def change_password(session: AsyncSession,
+                          user: User,
+                          old_password: str,
+                          new_password: str) -> bool:
+    if not verify_password(old_password, user.hashed_password):
+        raise ValueError("Invalid old password")
+
+    user.hashed_password = hash_password(new_password)
+    await session.commit()
+    await session.refresh(user)
+    return user
