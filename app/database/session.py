a823e2e063abf46f70bc04a46843fbb6c328ad1b
Morgrime <daniel2000@mail.ru>
2025-03-27 15:39:42 +0500

    Added setup.py

--- app/database/session.py
+++ app/database/session.py
@@ -1,11 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 import os
 
 
-# создаёт дб в папке database
-DATABASE_URL = f"sqlite+aiosqlite:///{os.path.join(os.getcwd(), 'app', 'database', 'database.db')}"
+load_dotenv()
 
+# создаёт дб в папке database
+# DATABASE_URL = f"sqlite+aiosqlite:///{os.path.join(os.getcwd(), 'app', 'database', 'database.db')}"
+# создаёт дб в папке с файлом main (если запуск был из той же директории)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(
