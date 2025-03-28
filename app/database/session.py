f27cef64f06d003384be61c40a2ae852cea481a1
Morgrime <daniel2000@mail.ru>
2025-03-28 11:05:17 +0500

    Change database location

--- app/database/session.py
+++ app/database/session.py
@@ -7,7 +7,10 @@ import os
 load_dotenv()
 
 # создаёт дб в папке database
-# DATABASE_URL = f"sqlite+aiosqlite:///{os.path.join(os.getcwd(), 'app', 'database', 'database.db')}"
+# DATABASE_URL = f"sqlite+aiosqlite:///{os.path.join(os.getcwd(),
+# 'app',
+# 'database',
+# 'database.db')}"
 # создаёт дб в папке с файлом main (если запуск был из той же директории)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
