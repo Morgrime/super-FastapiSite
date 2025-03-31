b32f971b7f88204a2fddda6a49d452febe2a634a
Morgrime <daniel2000@mail.ru>
2025-03-31 17:00:32 +0500

    Now using setting instead of dotenv

---
+++ app/database/config.py
@@ -0,0 +1,18 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_URL: str
+
+    @property
+    def DATABASE_URL_aiosqlite(self):
+        return f"{self.DB_URL}"
+
+    # Указываем путь к .env в родительской директории
+    model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), '..', '..', '.env'), extra="allow")
+
+
+settings = Settings()
+
+print(settings.DATABASE_URL_aiosqlite)
--- app/database/session.py
+++ app/database/session.py
@@ -1,20 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
+from app.database.config import settings
 
 
-load_dotenv()
-
-# создаёт дб в папке database
-# DATABASE_URL = f"sqlite+aiosqlite:///{os.path.join(os.getcwd(),
-# 'app',
-# 'database',
-# 'database.db')}"
-# создаёт дб в папке с файлом main (если запуск был из той же директории)
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL_aiosqlite, echo=True)
 SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine,
